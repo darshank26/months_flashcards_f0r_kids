0a5244883b0513036df9428a546a3a7d9239f0a0
darshan <darsh2605@gmail.com>
2024-01-06 17:00:43 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/darshankomu/months_flashcards_f0r_kids/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.darshankomu.months_flashcards_f0r_kids
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+const kprimarycolor = Color(0xff1C3249);
+const ksecondcolor = Color(0xff3ba2ff);
+const kthirdcolor = Color(0xff006ac0);
+const ksplashback = Color(0xffFF99D3);
+const kback = Color(0xffFF99D3);
+const backcolor = Color(0xFFFF99D3);
+const backcolor_2 = Color(0xFFFFFFFF);
+
+
+
+var androidAppShareLink = "https://play.google.com/store/apps/details?id=com.darshankomu.months_flashcards_f0r_kids";
+
+var androidShareText = "Download Months Planets for Kids app from below link";
+
+var androidAppIdValue = "com.darshankomu.months_flashcards_f0r_kids";
+
+var iOSAppIdValue = "AddiOSAppId";
